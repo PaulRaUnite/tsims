50452e302e134a5497dbbed82d62510c5ce0c5c7
Pavlo Tokariev <16649084+PaulRaUnite@users.noreply.github.com>
2018-05-20 15:52:19 +0300

    Better tape package names, documentation and empty tape setting bug fix.

--- tape/tape.go
+++ tape/tape.go
@@ -1,5 +1,8 @@
 package tape
 
+// Tape struct contains buffer of symbols,
+// empty symbol, current position of head, and
+// shift of zero position of the tape.
 type Tape struct {
 	buffer      []rune
 	zeroPos     uint64
@@ -7,7 +10,11 @@ type Tape struct {
 	emptySymbol rune
 }
 
+// Tape constructor.
 func Create(content string, emptySymbol rune) Tape {
+	if len(content) == 0 {
+		content = string(emptySymbol)
+	}
 	return Tape{
 		buffer:      []rune(content),
 		zeroPos:     0,
@@ -16,30 +23,37 @@ func Create(content string, emptySymbol rune) Tape {
 	}
 }
 
-type leftRight bool
+// "Enum" for enlarge to understand
+// what side of buffer must be enlarged.
+type side bool
 
+// "Enum"'s aliases.
 const (
-	left  leftRight = true
-	right leftRight = false
+	left  side = true
+	right side = false
 )
 
-const reallocationLength uint64 = 16
+// Constant for buffer enlarging.
+const enlargeLength uint64 = 16
 
-func (tape *Tape) reallocate(lr leftRight) {
+// enlarge method appends array of
+// empty symbols of `enlargeLength` length to
+// the side of the tape buffer.
+func (tape *Tape) enlarge(lr side) {
 	switch lr {
 	case left:
-		leftBuf := make([]rune, reallocationLength)
+		leftBuf := make([]rune, enlargeLength)
 		for i := range leftBuf {
 			leftBuf[i] = tape.emptySymbol
 		}
 		*tape = Tape{
 			buffer:      append(leftBuf, tape.buffer...),
-			zeroPos:     reallocationLength + tape.zeroPos,
-			currentPos:  reallocationLength + tape.currentPos,
+			zeroPos:     enlargeLength + tape.zeroPos,
+			currentPos:  enlargeLength + tape.currentPos,
 			emptySymbol: tape.emptySymbol,
 		}
 	case right:
-		rightBuf := make([]rune, reallocationLength)
+		rightBuf := make([]rune, enlargeLength)
 		for i := range rightBuf {
 			rightBuf[i] = tape.emptySymbol
 		}
@@ -47,31 +61,42 @@ func (tape *Tape) reallocate(lr leftRight) {
 	}
 }
 
-func (tape Tape) Now() rune {
-	if tape.currentPos == 0 && len(tape.buffer) == 0 {
-		return tape.emptySymbol
-	}
+// Returns symbol to which the head
+// of the tape are looking.
+func (tape Tape) HeadSymbol() rune {
 	return tape.buffer[tape.currentPos]
 }
 
-func (tape *Tape) LeftShift() {
+// Shifts head to left.
+// Adds additional space to buffer
+// if it exceeds the buffer's
+// limit to its left side.
+func (tape *Tape) HeadToLeft() {
 	if tape.currentPos == 0 {
-		tape.reallocate(left)
+		tape.enlarge(left)
 	}
 	tape.currentPos--
 }
 
-func (tape *Tape) RightShift() {
+// Shifts head to right.
+// Adds additional space to buffer
+// if it exceeds the buffer's
+// limit to its right side.
+func (tape *Tape) HeadToRight() {
 	tape.currentPos++
 	if tape.currentPos == uint64(len(tape.buffer)) {
-		tape.reallocate(right)
+		tape.enlarge(right)
 	}
 }
 
-func (tape Tape) Set(symbol rune) {
+// Sets symbol to head position.
+func (tape *Tape) Set(symbol rune) {
 	tape.buffer[tape.currentPos] = symbol
 }
 
+// Returns debug representation of tape
+// in the form of string of the tape's
+// content and head position.
 func (tape Tape) String() string {
 	top := string(tape.buffer)
 	pos := make([]rune, tape.currentPos+1, len(tape.buffer)+1)
